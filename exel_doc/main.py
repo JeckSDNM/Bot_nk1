131a421d3963759ebbae6759cb739678486fcaa2
JeclSDNM <bezdomnuygod@mail.ru>
2021-11-26 18:58:23 +0700

    File for Test -v:3

--- exel_doc/main.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-
-exel = pd.read_excel('Kopia_Kopia_NOVYE_Vygruzka_SPO_GPOU_Kuznetskiy_industrialny_tekhnikum_1546sht.xlsx', engine='openpyxl')
-exel = exel.head(5)
-
--- main.py
+++ main.py
@@ -17,13 +17,14 @@ class Bot:
 
     def __init__(self):
         Bot.__emp_count += 1
-        self.__exelDoc = ''
-        self.__get_code_list()
+        self.__data_frame = self.__check_exel_doc()
+        self.__is_exelDoc = self.__get_path()
+
         self.__student_code = ''
         self.__result = ''
-        print('Бот создан')
-        self.__check_exel_doc()
         self.driver = None
+
+        print('Бот создан')
 
     @staticmethod
     def __interval():
@@ -38,37 +39,10 @@ class Bot:
             try:
                 return webdriver.Firefox()
             except:
-                print("Не найден подходязий браузер")
+                print("Не найден подходязий браузер. Версия драйвера могла устареть.")
                 return sys.exit()
 
-    def __get_path(self):
-        path_exel_doc = input('Введите путь к таблице логинов(если есть): \n').strip()
-        if os.path.exists(path_exel_doc):
-            self.__exelDoc = path_exel_doc
-
-        elif len(path_exel_doc) < 3 and path_exel_doc in ['', ' ']:
-            self.__exelDoc = ''
-        else:
-            print('Файла не существует.')
-            self.__get_path()
-
-
-    # ######################################## Работа со списком exel
-    def __get_code_list(self):
-        if self.__exelDoc != '':
-            # self.__check_exel_doc()
-            exel = pd.read_excel(self.__exelDoc)
-            log_list1 = exel['Логин'].tolist()
-            log_list2 = self.__data_frame['Логин'].tolist()
-            self.__code_list = list((set(log_list1) - set(log_list2))) + list((set(log_list2) - set(log_list1)))
-            self.__index_code = -1
-            self.__max_index_code = len(self.__code_list)
-
-    def __get_student_code(self):
-        self.__index_code += 1
-        return self.__code_list[self.__index_code]
-
-    # #######################################  Сохранение результатов
+    '''Проверка/создание дока для сохранения результатов'''
     def __check_exel_doc(self):
         if not os.path.isdir("exel_doc"):
             os.mkdir("exel_doc")
@@ -77,27 +51,60 @@ class Bot:
             d = {'Номер': [], 'Логин': [], 'Результат': []}
             pd.DataFrame(data=d).to_excel(f'exel_doc/Result{Bot.__emp_count}.xlsx')
 
-        self.__data_frame = pd.read_excel(f'exel_doc/Result{Bot.__emp_count}.xlsx', index_col=0)
+        return pd.read_excel(f'exel_doc/Result{Bot.__emp_count}.xlsx', index_col=0)
 
+    # ######################################## Работа со списком exel
+    '''Задает exel документ'''
+    def __get_path(self):
+        path_exel_doc = input('Введите путь к таблице логинов(пропустиде для ручного ввода): ').strip()
+        if os.path.exists(path_exel_doc):
+            self.__exelDoc = path_exel_doc
+            self.__get_code_list()
+            return True
+
+        elif len(path_exel_doc) < 4:
+            return False
+        else:
+            print('Файла не существует.')
+            self.__get_path()
+
+    '''Выборка столбца логинов если есть файл'''
+    def __get_code_list(self):
+        exel = pd.read_excel(self.__exelDoc)
+        name_column = 'Лгин'
+        if name_column not in exel.columns:
+            while name_column not in exel.columns:
+                name_column = input('Не найден столбец логинов.\nВведите название столбца.\n=>')
+
+        log_list1 = exel[name_column].tolist()
+        log_list2 = self.__data_frame['Логин'].tolist()
+        self.__code_list = list((set(log_list1) - set(log_list2))) + list((set(log_list2) - set(log_list1)))
+        self.__index_code = -1
+        # self.__max_index_code = len(self.__code_list)
+
+    # ######################################################## сохранение
+    '''Сохранение данных в файл отсчета'''
     def __saved_data(self):
         self.__data_frame = self.__data_frame.append({'Логин': self.__student_code, 'Результат': self.__result}, ignore_index=True)
         self.__data_frame.to_excel(f'exel_doc/Result{Bot.__emp_count}.xlsx')
 
     # ######################################################## Основа
-    def __up_link(self):
-        self.driver.get('https://spt.kuzrc.ru/')
+    def __get_student_code(self):
+        self.__index_code += 1
+        return self.__code_list[self.__index_code]
 
     def __set_student_code(self):
-        if self.__exelDoc == '':
-            self.__student_code = input('Введите код студента: ')
-        else:  # метод для пандас таблицы
+        if self.__is_exelDoc:
             self.__student_code = self.__get_student_code()
+        else:
+            self.__student_code = input('Введите код студента: ')
 
+    '''Вводит код студента'''
     def __inp_code(self):
+        self.__set_student_code()
         elem = self.driver.find_element(By.NAME, 'oLogin')
         elem.send_keys(self.__student_code)
         self.driver.find_element(By.TAG_NAME, 'button').click()
-
         try:
             self.driver.find_element(By.XPATH, '/html/body/div/main/div/div/div/div/div/h3[2]')
             logic = True
@@ -108,9 +115,9 @@ class Bot:
             print(self.__student_code, ': Код не верный или уже используется')
             self.__result = 'Неверный'
             self.__saved_data()
-            self.__set_student_code()
             self.__inp_code()
 
+    '''Проходит 2ю и 3ю страницы'''
     def __sec_the_pages(self):
         self.driver.find_element(By.NAME, 'sex').click()
         elem = self.driver.find_element(By.NAME, 'age')
@@ -122,10 +129,7 @@ class Bot:
         self.driver.find_element(By.TAG_NAME, 'button').click()
         sleep(1)
 
-    def __login(self):
-        self.__inp_code()
-        self.__sec_the_pages()
-
+    '''Неубиваемый кусок, всегда работает'''
     def __test(self):
         text_q = self.driver.find_element(By.CLASS_NAME, 'test_block').find_element(By.TAG_NAME, 'div').text
         but_arr = self.driver.find_element(By.CLASS_NAME, 'answers').find_elements(By.TAG_NAME, 'button')
@@ -142,6 +146,7 @@ class Bot:
                 print('Нет в ', k)
                 but_arr[-3].click()
 
+    '''Устанавливает результат'''
     def __set_result(self):
         result = self.driver.find_element(By.XPATH, '/html/body/div[2]/main/div[1]/div/div/div/div[2]/div[2]/div[1]')
         if result.text.find('Вы успешно прошли социально-психологический тест.') != -1:
@@ -150,12 +155,17 @@ class Bot:
             a = 'Провал'
         self.__result = 'Пройдено' + a
 
+    def __up_link(self):
+        self.driver.get('https://spt.kuzrc.ru/')
+
     def _start(self):
         if self.driver is None:
             self.driver = self.__start_driver()
         self.__up_link()
-        self.__set_student_code()
-        self.__login()
+
+        self.__inp_code()
+        self.__sec_the_pages()
+
         for i in range(140):
             self.__test()
         sleep(0.5)
@@ -163,25 +173,6 @@ class Bot:
         self.__set_result()
         self.__saved_data()
 
-    def main(self):
-        if self.__exelDoc != '':
-            nm_repeat = 0
-            try:
-                nm_repeat = int(input('Введите число повторений: '))
-            except:
-                print('Значение должно быть числом')
-                self.main()
-
-            for i in range(nm_repeat):
-
-                self._start()
-        else:
-            self._start()
-
-
-name_exel_doc = input('Введите путь к таблице кодов(если есть): \n')
-# input('Введите путь к таблице кодов(если есть): \n')
-# 'exel_doc/Kopia_Kopia_NOVYE_Vygruzka_SPO_GPOU_Kuznetskiy_industrialny_tekhnikum_1546sht.xlsx'
 
 bot1 = Bot()
-bot1.main()
+bot1._start()
